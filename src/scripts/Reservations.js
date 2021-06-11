dada44fdb74a94ec8656b911ab637e9a01d50e11
Sam Carbine <scarbine@me.com>
2021-06-11 15:30:40 -0500

    list items in order

--- src/scripts/Reservations.js
+++ src/scripts/Reservations.js
@@ -1,15 +1,34 @@
-import { getReservations, deleteReservation, completeReservation } from "./dataAccess.js";
+import {
+  getReservations,
+  deleteReservation,
+  completeReservation,
+} from "./dataAccess.js";
 
 export const Reservations = () => {
-  const reservations = getReservations();
-  let html = "<ul>"
-  const listReservationItems = reservations.map((reservation) => {
-    return `<li class="reservation_item" > <div class="reservation_list_item" id="reservation--${reservation.id}"> ${reservation.childName} has a party on ${reservation.date} and should last ${reservation.duration} hours. ${reservation.numberOfChildren} kids are expected at the party.</div> 
+  const reservations = getReservations()
+  const sortedReservations = reservations.sort((a,b) => {
+
+    return parseInt(a.date.split("-").join("")) - parseInt(b.date.split("-").join(""))
+  })
+  let html = "<ul>";
+
+  // capture the value of the date
+  // split the value of the date at -
+  // join the value of the date with .join ""
+  // parseInt the value of the join
+  // sort the values by lowest to highest
+
+  const listReservationItems = sortedReservations.map((reservation) => {
+    const dateSplit = reservation.date.split("-");
+    const dateAsString = dateSplit.join("");
+
+    return `<li class="reservation_item" > <div value="${dateAsString}"class="reservation_list_item" id="reservation--${reservation.id}"> ${reservation.childName} has a party on ${reservation.date} and should last ${reservation.duration} hours. ${reservation.numberOfChildren} kids are expected at the party.</div> 
     <button id="reservation--${reservation.id}" name="delete_button" class="delete_order_button">Delete</button> 
-    <button id="complete--${reservation.id}" name="complete_button" class="complete_order_button">Complete</button></li>`
+    <button id="complete--${reservation.id}" name="complete_button" class="complete_order_button">Complete</button></li>`;
   });
-  html += listReservationItems.join("")
-  html += "</ul>"
+
+  html += listReservationItems.join("");
+  html += "</ul>";
   return html;
 };
 
@@ -18,18 +37,15 @@ export const Reservations = () => {
 const mainContainer = document.querySelector("#container");
 
 mainContainer.addEventListener("click", (event) => {
-	if (event.target.id.startsWith("reservation--")){
-		const [,reservationId] = event.target.id.split("--")
-		deleteReservation(parseInt(reservationId))
-
-	}
-})
-
-
+  if (event.target.id.startsWith("reservation--")) {
+    const [, reservationId] = event.target.id.split("--");
+    deleteReservation(parseInt(reservationId));
+  }
+});
 
 mainContainer.addEventListener("click", (event) => {
-  if (event.target.id.startsWith("complete--")){
-    const [,reservationId] = event.target.id.split("--")
-    completeReservation(parseInt(reservationId))
+  if (event.target.id.startsWith("complete--")) {
+    const [, reservationId] = event.target.id.split("--");
+    completeReservation(parseInt(reservationId));
   }
-})
+});
